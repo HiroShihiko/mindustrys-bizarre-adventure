90802a005382736dffd52755d5600c91aa6cd82b
GlennFolker <63218676+GlennFolker@users.noreply.github.com>
2020-06-28 14:36:13 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
-require("mindustrys-bizzare-adventure/turrets/arccaster")
+require("mindustrys-bizzare-adventure/turrets/arccaster");
+
+const initialize = function(){
+	Core.assets.load("music/mindustrys-bizzare-adventure/menu.ogg", Packages.arc.audio.Music).loaded = cons(a => Musics.menu = a);
+}
+initialize();
