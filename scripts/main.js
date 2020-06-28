afa5b4f226d34265b94778218973cbbbc9ec935a
GlennFolker <glenn.folk08@gmail.com>
2020-06-28 18:28:49 +0700

    Mod update

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
-require("mindustrys-bizzare-adventure/turrets/arccaster");
+require("mindustrys-bizarre-adventure/turrets/arccaster");
 
 const initialize = function(){
-	Core.assets.load("sounds/menu.ogg", Packages.arc.audio.Music).loaded = cons(a => Musics.menu = a);
+	Core.assets.load(
+		"sounds/menu.ogg",
+		Packages.arc.audio.Music
+	).loaded = cons(a => Musics.menu = a);
 }
 initialize();
